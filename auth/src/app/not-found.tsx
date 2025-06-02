5d5e4486a9ddc34df6ddc36ad4344dcedefc7d7a
SMg <soulmasterg1084@gmail.com>
2025-06-01 23:37:29 -0400

    Update not-found.tsx

--- auth/src/app/not-found.tsx
+++ auth/src/app/not-found.tsx
@@ -1,9 +1,12 @@
 // src/app/not-found.tsx
+export const metadata = {
+  title: '404',
+};
 export default function NotFound() {
   return (
     <div>
       <h1>404 - Page Not Found</h1>
-      <p>You will be redirected to the homepage...</p>
+      <p>You will be redirected to the Fork Page...</p>
       <script
         dangerouslySetInnerHTML={{
           __html: `setTimeout(() => { window.location.href = '/' }, 1000);`
